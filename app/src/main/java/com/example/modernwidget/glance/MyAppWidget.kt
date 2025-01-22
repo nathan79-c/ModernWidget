8c35e9964a06dfe9e11b4f46535adbbbbc80791d
nathan79-c <19kt210@gmail.com>
2025-01-22 11:36:35 +0200

    remove unsed directive

--- app/src/main/java/com/example/modernwidget/glance/MyAppWidget.kt
+++ app/src/main/java/com/example/modernwidget/glance/MyAppWidget.kt
@@ -1,36 +1,28 @@
 package com.example.modernwidget.glance
 
 import android.content.Context
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
-import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
-import androidx.glance.layout.Row
-import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
-import androidx.glance.layout.width
 import androidx.glance.text.Text
 import androidx.glance.text.TextAlign
 import androidx.glance.text.TextStyle
-import com.example.modernwidget.MainActivity
 import kotlinx.coroutines.delay
 
 class MyAppWidget : GlanceAppWidget() {
@@ -54,7 +46,7 @@ private fun MyContent() {
         "The joy of the Lord is your strength. - Nehemiah 8:10"
     )
 
-    var currentVerseIndex by remember { mutableStateOf(0) }
+    var currentVerseIndex by remember { mutableIntStateOf(0) }
 
     // Change the verse every few seconds
     LaunchedEffect(Unit) {
